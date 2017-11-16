77cf8bfcde1b3f59af6aa61b58ad90c7a1da0179
github-jason <jason.seekjob@gmail.com>
2017-11-17 10:40:50 +1100

    add random alphanumeric script
    1. add random apphanumeric script for future use;
    2. modify index.html

---
+++ js/randalphanumeric.js
@@ -0,0 +1,20 @@
+/**
+ * RANDOM STRING GENERATOR
+ *
+ * Info:      http://stackoverflow.com/a/27872144/383904
+ * Use:       randomString(length [,"A"] [,"N"] );
+ * Default:   return a random alpha-numeric string
+ * Arguments: If you use the optional "A", "N" flags:
+ *            "A" (Alpha flag)   return random a-Z string
+ *            "N" (Numeric flag) return random 0-9 string
+ */
+function randomString(len, an){
+    an = an&&an.toLowerCase();
+    var str="", i=0, min=an=="a"?10:0, max=an=="n"?10:62;
+    for(;i++<len;){
+      var r = Math.random()*(max-min)+min <<0;
+      str += String.fromCharCode(r+=r>9?r<36?55:61:48);
+    }
+    return str;
+}
+
