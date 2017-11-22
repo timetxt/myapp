c2b1ed15bc868445905652bfb80c5ece03747e71
github-jason <jason.seekjob@gmail.com>
2017-11-22 17:05:29 +1100

    generate 8 charactors random string
    generate 8 charactors random string

--- js/randalphanumeric.js
+++ js/randalphanumeric.js
@@ -15,6 +15,6 @@ function randomString(len, an){
       var r = Math.random()*(max-min)+min <<0;
       str += String.fromCharCode(r+=r>9?r<36?55:61:48);
     }
-    return str;
+    document.getElementById("randomstring").value = str;
 }
 
